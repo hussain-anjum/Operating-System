6f62948e19b4ac377a107b4a271284d66924fa1b
hussain-anjum <haratul14@gmail.com>
2025-11-30 00:00:38 +0600

    Add ifstream to read input from .txt

--- FCFS/FCFS_without_arrival_Time.cpp
+++ FCFS/FCFS_without_arrival_Time.cpp
@@ -1,63 +1,81 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+struct Process
+{
+    string pid;
+    int bt, ct, tat, wt;
+};
+
 int main()
 {
+    ifstream infile("FCFS2.txt");
+
     int n;
-    cout << "Enter number of processes: ";
-    cin >> n;
-    if (n <= 0)
-    {
-        cerr << "Invalid number of processes" << endl;
-        return 1;
-    }
+    infile >> n;
 
-    int bt[n], wt[n];
-
+    vector<Process> processes(n);
     for (int i = 0; i < n; i++)
     {
-        cout << "Enter burst time for P" << i << ": ";
-        cin >> bt[i];
-        if (bt[i] < 0)
+        infile >> processes[i].bt;
+        if (processes[i].bt < 0)
         {
             cerr << "Burst time must be non-negative" << endl;
             return 1;
         }
+        processes[i].pid = "P" + to_string(i);
     }
+    // infile.close();
 
-    int total_tat = 0;
-    for (int i = 0; i < n; i++)
-        total_tat += bt[i];
-
-    wt[0] = 0;
-    for (int i = 1; i < n; i++)
-        wt[i] = wt[i - 1] + bt[i - 1];
-
-    int total_wt = 0;
-    for (int i = 0; i < n; i++)
-        total_wt += wt[i];
-
-    double avg_wt = static_cast<double>(total_wt) / n;
-
-    cout << "\nProcess\t\tBurst Time\tWaiting Time" << endl;
-    for (int i = 0; i < n; ++i)
+    queue<Process> ready_queue;
+    for (auto &p : processes)
     {
-        cout << "P" << i << "\t\t" << bt[i] << "\t\t" << wt[i] << endl;
+        ready_queue.push(p);
     }
 
-    cout << "\nGantt Chart: " << endl;
-    for (int i = 0; i < n; i++)
+    vector<Process> executed;
+    vector<pair<string, int>> gantt_chart;
+    int current_time = 0;
+
+    while (!ready_queue.empty())
     {
-        cout << "P" << i;
-        if (i != n - 1)
-            cout << " -> ";
-    }
-    cout << endl;
+        Process p = ready_queue.front();
+        ready_queue.pop();
 
-    cout << "\nTotal turn around time: " << total_tat << endl;
-    cout << "Total Waiting Time: " << total_wt << endl;
-    cout << "Average Waiting Time: " << fixed << setprecision(2) << avg_wt << endl;
-    cout << endl;
+        gantt_chart.push_back({p.pid, p.bt});
+        current_time += p.bt;
+
+        p.ct = current_time;
+        p.tat = p.ct;
+        p.wt = p.tat - p.bt;
+
+        executed.push_back(p);
+    }
+
+    cout << "\nGantt Chart: ";
+    for (size_t i = 0; i < gantt_chart.size(); i++)
+    {
+        cout << gantt_chart[i].first;
+        if (i != gantt_chart.size() - 1)
+            cout << " --> ";
+    }
+    cout << "\n";
+
+    cout << "\nProcess\tBT\tCT\tTAT\tWT\n";
+    int total_tat = 0, total_wt = 0;
+
+    for (auto &p : executed)
+    {
+        cout << p.pid << "\t" << p.bt << "\t" << p.ct
+             << "\t" << p.tat << "\t" << p.wt << "\n";
+        total_tat += p.tat;
+        total_wt += p.wt;
+    }
+
+    cout << "\nTotal Turnaround Time: " << total_tat;
+    cout << "\nTotal Waiting Time: " << total_wt;
+    cout << "\nAverage Turnaround Time: " << (double)total_tat / n;
+    cout << "\nAverage Waiting Time: " << (double)total_wt / n;
 
     return 0;
 }
